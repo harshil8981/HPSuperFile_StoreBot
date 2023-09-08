910a99eb812f4384fc3abe81b27e63de94351aa6
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-09-08 17:44:44 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,59 @@
+# (c) @harshil8981 || @Hpbot_update 
+
+import os
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID", "0"))
+	API_HASH = os.environ.get("API_HASH")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
+	SHORTLINK_API = os.environ.get('SHORTLINK_API')
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	DATABASE_URL = os.environ.get("DATABASE_URL")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	ABOUT_BOT_TEXT = f"""
+This is a Permanent FileStore Bot. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
+│
+├🔸 **My Name:** [FileStore Bot](https://t.me/{BOT_USERNAME})
+│
+├🔸 **Language:** [Python 3](https://www.python.org)
+│
+├🔹 **Library:** [Pyrogram](https://docs.pyrogram.org)
+│
+├🔹 **Hosted On:** [Heroku](https://heroku.com)
+│
+├🔸 **Developer:** [𝐌𝐑𝐊𝐈𝐋𝐋𝐄𝐑 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑](https://t.me/Mrkiller_1109) 
+│
+├🔹 **Bot Support:** [Support Group](https://t.me/HP_Bot_discuss_group)
+│
+├🔸 **Bot Updates:** [Bots Channel](https://t.me/Hpbot_update)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@Mrkiller_1109](https://github.com/harshil8981)
+ 
+ I am Super noob Please Support My Hard Work.
+"""
+	HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+
+How to Use Bot & it's Benefits??
+
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+
+❌ **PORNOGRAPHY CONTENTS** are strictly prohibited & get Permanent Ban.
+"""
