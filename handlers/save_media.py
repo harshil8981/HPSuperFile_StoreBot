a65b4e908289b0eb8947bb6fb8d9b52cb7100d29
Rplay <88985252+Renishrplay@users.noreply.github.com>
2024-08-11 17:14:25 +0100

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -49,7 +49,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
                 InlineKeyboardButton("Delete Batch", callback_data="closeMessage")
             ]])
         )
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=mrkiller_{str_to_b64(str(SaveMessage.id))}"
+        share_link = f"{Config.BOT_USERNAME}?start=mrkiller_{str_to_b64(str(SaveMessage.id))}"
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
             f"Just Click the link to get your files!",
