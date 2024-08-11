8bc2ea1e21bfc9b6f2acd68d36cac5f01ae15316
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-08-11 21:45:51 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -87,7 +87,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://t.me/{Config.BOT_USERNAME}?start=mrkiller_{str_to_b64(file_er_id)}"
+        share_link = f"{Config.BOT_USERNAME}?start=mrkiller_{str_to_b64(file_er_id)}"
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
             f"Here is the Permanent Link of your file: {share_link} \n\n"
